a50ee568bdadef7e114ed629d542c82883c51ed0
Sojisoyoye <soji.soyoye@gmail.com>
2020-10-03 10:12:48 +0100

    add rinkeby network to deploy

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # proof-of-existence
+
+Smart contracts based on this article.
+
+`https://blog.openzeppelin.com/the-hitchhikers-guide-to-smart-contracts-in-ethereum-848f08001f05/`
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
-var ProofOfExistence2 = artifacts.require('./ProofOfExistence2.sol');
+var ProofOfExistence3 = artifacts.require('./ProofOfExistence3.sol');
+// var ProofOfExistence2 = artifacts.require('./ProofOfExistence2.sol');
+// var ProofOfExistence1 = artifacts.require('./ProofOfExistence1.sol');
 
 module.exports = function(deployer) {
-    deployer.deploy(ProofOfExistence2);
+    deployer.deploy(ProofOfExistence3);
 }
